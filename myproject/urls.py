d83b280b3c47d8be6998f782f0b11cf312b9d2f1
Yasmine Sidi Mansour <yasmine.sidimansour@univ-constantine2.dz>
2025-11-25 20:09:56 +0100

    save

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('students/', include('students.urls')),
     path('university/', include('university.urls')),
